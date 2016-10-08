408031536a6c95675cb7f155f87e9e629f477038
Sam <zhang.song@successinfo.com.cn>
2016-10-09 00:52:56 +0800

    update samples.

---
+++ test_objectbuf.lua
@@ -0,0 +1,51 @@
+local objectbuf = require "fan.objectbuf"
+
+ttt = "vvv"
+
+local a = {
+    -- foo = function(self, x, y)
+    --     for i,v in ipairs(self.b) do
+    --         print(i,v)
+    --     end
+    --     return math.random(x, y), ttt
+    -- end,
+    b = {
+        1234556789,
+        12345.6789,
+        nil,
+        -1234556789,
+        -12345.6789,
+        0,
+        function()end
+    }
+}
+
+a.b.a = a
+
+for i=1,100 do
+    table.insert(a.b, "123456789012345678901234567890")
+end
+
+-- local cjson = require "cjson"
+
+local buf = objectbuf.encode(a)
+-- print("#(buf)", #(buf), #(cjson.encode(a)))
+
+local f = io.open("buf.bin", "wb")
+f:write(buf)
+f:close()
+-- os.exit()
+
+local f = io.open("buf.bin", "rb")
+local buf = f:read("*all")
+f:close()
+
+local obj = objectbuf.decode(buf)
+
+for i=1,10 do
+    print(i, obj.b[i])
+end
+
+print(type(nil))
+
+-- print(obj.b.a:foo(100, 200))
--- test_worker.lua
+++ test_worker.lua
@@ -2,18 +2,18 @@ local fan = require "fan"
 local worker = require "fan.worker"
 
 local commander = worker.new({
-    ["test"] = function(x, y)
-        return math.random(x, y), nil, y, x
+    ["test"] = function(x, n, y, s)
+        return n, math.random(x, y), nil, y, x, s
     end
 }, 2)
 
 fan.loop(function()
-    fan.sleep(2)
+    fan.sleep(1)
     for k,v in pairs(commander.slaves) do
         print("commander.slaves",k,v)
     end
     while true do
-        print(commander:test(1000, 10000))
+        print(commander:test(1000, nil, 10000, "sss"))
         fan.sleep(1)
     end
 end)
